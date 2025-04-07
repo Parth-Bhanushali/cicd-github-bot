989e06f28196e7cd18413beac83c43c42c0d336d
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2025-04-07 17:28:46 +0530

    Listen to issues reopened instead of opened

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import { Probot } from "probot";
 import { handlePreviewDeploymentsEvent } from './features/preview-deployments/index.js';
 
 export default (app: Probot) => {
-  app.on("issues.opened", async (context) => {
+  app.on("issues.reopened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Thanks for reopening this issue!",
     });
     await context.octokit.issues.createComment(issueComment);
   });
